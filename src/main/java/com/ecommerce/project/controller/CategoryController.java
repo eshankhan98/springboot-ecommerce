8933e620edd07008501d4de6acf641b65637f394
Eshan Khan <khaneshan.33843@gmail.com>
2026-07-11 11:25:53 +0530

    Code updated

--- src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -1,43 +1,53 @@
 package com.ecommerce.project.controller;
 
 import com.ecommerce.project.model.Category;
+import com.ecommerce.project.payload.CategoryDTO;
+import com.ecommerce.project.payload.CategoryResponse;
 import com.ecommerce.project.service.CategoryService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
+
 @RequestMapping("/api")
 public class CategoryController {
 
     @Autowired
     private CategoryService categoryService;
 
-    @GetMapping("/public/categories")
-    public ResponseEntity<List<Category>> getAllCategories(){
+    @GetMapping("/echo")
+    public ResponseEntity<String> echoMessage(@RequestParam("message") String message){
+        return new ResponseEntity<>("Echoed message: "+ message,HttpStatus.OK);
+    }
 
-        List<Category> categories = categoryService.getAllCategories();
-        return new ResponseEntity<>(categories, HttpStatus.OK);
+    @GetMapping("/public/categories")
+    public ResponseEntity<CategoryResponse> getAllCategories(
+            @RequestParam(name="pageNumber") Integer pageNumber,
+            @RequestParam(name="pageSize") Integer pageSize
+    ){
+        CategoryResponse categoryResponse= categoryService.getAllCategories(pageNumber,pageSize);
+        return new ResponseEntity<>(categoryResponse, HttpStatus.OK);
     }
 
     @PostMapping("/public/categories")
-    public ResponseEntity<String> createCategory(@RequestBody Category category){
-        categoryService.createCategory(category);
-        return new ResponseEntity<>("Category added successfully",HttpStatus.CREATED);
+    public ResponseEntity<CategoryDTO> createCategory(@Valid @RequestBody CategoryDTO categoryDTO){
+        CategoryDTO savedCategoryDTO = categoryService.createCategory(categoryDTO);
+        return new ResponseEntity<>(savedCategoryDTO ,HttpStatus.CREATED);
     }
 
     @DeleteMapping("/admin/categories/{categoryId}")
-    public ResponseEntity<String> deleteCategory(@PathVariable Long categoryId){
-        try {
-            String status = categoryService.deleteCategory(categoryId);
-            return new ResponseEntity<>(status, HttpStatus.OK);
-        }catch (ResponseStatusException e){
-            return new ResponseEntity<>(e.getReason(), e.getStatusCode());
-        }
+    public ResponseEntity<CategoryDTO> deleteCategory(@PathVariable Long categoryId){
+           CategoryDTO deleteCategory = categoryService.deleteCategory(categoryId);
+            return new ResponseEntity<>(deleteCategory,HttpStatus.OK);
+    }
+    @PutMapping("/public/categories/{categoryId}")
+    public ResponseEntity<CategoryDTO> updateCategory(
+            @Valid @RequestBody CategoryDTO categoryDTO,
+            @PathVariable Long categoryId) {
+        CategoryDTO updatedCategory = categoryService.updateCategory(categoryDTO, categoryId);
+        return new ResponseEntity<>(updatedCategory, HttpStatus.OK);
     }
 }
--- src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -1,36 +1,71 @@
 package com.ecommerce.project.service;
 
+import com.ecommerce.project.exception.APIException;
+import com.ecommerce.project.exception.ResourceNotFoundException;
 import com.ecommerce.project.model.Category;
-import org.springframework.http.HttpStatus;
+import com.ecommerce.project.payload.CategoryDTO;
+import com.ecommerce.project.payload.CategoryResponse;
+import com.ecommerce.project.repositories.CategoryRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class CategoryServiceImpl implements CategoryService{
-    private List<Category> categories = new ArrayList<>();
-    private Long nextId = 1L;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Autowired
+    private ModelMapper modelMapper;
 
     @Override
-    public List<Category> getAllCategories() {
-        return categories;
+    public CategoryResponse getAllCategories(Integer pageNumber,Integer pageSize) {
+        Pageable pageDetails = PageRequest.of(pageNumber, pageSize);
+        Page<Category> categoryPage= categoryRepository.findAll(pageDetails);
+        List<Category> categories = categoryRepository.findAll();
+        if(categories.isEmpty())
+            throw new APIException("No category created till now.");
+        List<CategoryDTO> categoryDTOS = categories.stream()
+                .map(category -> modelMapper.map(category, CategoryDTO.class))
+                .toList();
+        CategoryResponse categoryResponse = new CategoryResponse();
+        categoryResponse.setContent(categoryDTOS);
+        return categoryResponse;
     }
 
     @Override
-    public void createCategory(Category category) {
-        category.setCategoryId(nextId++);
-        categories.add(category);
+    public CategoryDTO createCategory(CategoryDTO categoryDTO) {
+        Category category = modelMapper.map(categoryDTO, Category.class);
+        Category categoryFromDb = categoryRepository.findByCategoryName(category.getCategoryName());
+        if (categoryFromDb !=null){
+            throw new APIException("Category with the name "+category.getCategoryName()+" already exists !");
+        }
+        Category savedCategory = categoryRepository.save(category);
+        return modelMapper.map(savedCategory, CategoryDTO.class);
     }
 
     @Override
-    public String deleteCategory(Long categoryId) {
-        Category category = categories.stream()
-                .filter(c->c.getCategoryId().equals(categoryId))
-                .findFirst()
-                .orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND,"Resource not Found"));
+    public CategoryDTO deleteCategory(Long categoryId) {
+        Category category= categoryRepository.findById(categoryId)
+                .orElseThrow(()-> new ResourceNotFoundException("Category","categoryId",categoryId));
+        categoryRepository.delete(category);
+        return modelMapper.map(category, CategoryDTO.class);
+    }
 
-        categories.remove(category);
-        return "Category with categoryId : " + categoryId + " deleted successfully !! ";
+    @Override
+    public CategoryDTO updateCategory(CategoryDTO categoryDTO, Long categoryId){
+       Category savedCategory = categoryRepository.findById(categoryId)
+               .orElseThrow(()->new ResourceNotFoundException("Category","categoryId",categoryId));
+       Category category = modelMapper.map(categoryDTO,Category.class);
+       category.setCategoryId(categoryId);
+       savedCategory = categoryRepository.save(category);
+       return modelMapper.map(savedCategory, CategoryDTO.class);
     }
 }
